2edeca3254475d7f0b4f109ed4f7a7e40efef9bb
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-05-11 10:39:59 -0600

    Creacion de las clases respectivas del paquete Factory

---
+++ src/ejercicio2/factory/Figure.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2.factory;
+
+/**
+ * @author Andrés Antonio Gonzalez Orozco B83477
+ * @author José David Camacho Vargas B91484
+ */
+public abstract class Figure {
+    
+}

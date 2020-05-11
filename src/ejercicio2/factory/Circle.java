303424438584be27f2b13852c273ad3c8e09ee1a
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-05-11 11:08:08 -0600

    Creacion de la clase FactoryException y modificaciones en la clase Circle

--- src/ejercicio2/factory/Circle.java
+++ src/ejercicio2/factory/Circle.java
@@ -5,10 +5,44 @@
  */
 package ejercicio2.factory;
 
+import ejercicio1.singletonpattern.DeliveredPurchasesList;
+
 /**
  * @author Andrés Antonio Gonzalez Orozco B83477
  * @author José David Camacho Vargas B91484
  */
 public class Circle {
+    private int centralPoint;
+    private int ratioDistance;
+
+    public Circle(int centralPoint, int ratioDistance) {
+        this.centralPoint = centralPoint;
+        this.ratioDistance = ratioDistance;
+    }
+
+    public int getCentralPoint() {
+        if (centralPoint < 0) {
+            
+        }
+        return centralPoint;
+    }
+
+    public void setCentralPoint(int centralPoint) {
+        this.centralPoint = centralPoint;
+    }
+
+    public int getRatioDistance() {
+        return ratioDistance;
+    }
+
+    public void setRatioDistance(int ratioDistance) {
+        this.ratioDistance = ratioDistance;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo ->" + " Punto central: " + centralPoint + ", distancia de radio: " + ratioDistance;
+    }
+    
     
 }
---
+++ src/ejercicio2/factory/FactoryException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2.factory;
+
+/**
+ *
+ * @author Andres Gonzalez
+ */
+public class FactoryException extends Exception {
+
+    public FactoryException(String message) {
+        super(message);
+    }
+    
+}

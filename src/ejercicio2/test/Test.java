eb468cf26d71e9122b45dd18f572ccce4d9310e9
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-05-18 01:16:12 -0600

    Modificaciones en Test

--- src/ejercicio2/test/Test.java
+++ src/ejercicio2/test/Test.java
@@ -5,6 +5,15 @@
  */
 package ejercicio2.test;
 
+import ejercicio2.composite.MixedFigure;
+import ejercicio2.composite.MuralComponent;
+import ejercicio2.factory.CircleFactory;
+import ejercicio2.factory.FactoryException;
+import ejercicio2.factory.Figure;
+import ejercicio2.factory.FigureFactory;
+import ejercicio2.factory.RectangleFactory;
+import java.awt.Point;
+
 /**
  * Prueba del ejercicios 2
 * @author Andrés Antonio Gonzalez Orozco B83477
@@ -12,6 +21,19 @@ package ejercicio2.test;
  */
 public class Test {
     public static void main (String [] args){
+        // Prueba Circulo
         
+        FigureFactory factory = new CircleFactory();
+        Figure circle = factory.createFigure();
+        circle.diference();
+        System.out.println(circle.toString());
+        
+        // Prueba Rectangulo
+        FigureFactory factory1 = new RectangleFactory();
+        Figure rectangle = factory1.createFigure();
+        rectangle.diference();
+        System.out.println(" " + rectangle.diference());
+        System.out.println(rectangle.toString());
+//        MuralComponent mc = new MuralComponent(figures);
     } 
 }
